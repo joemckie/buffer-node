146769613ad13e2cdd66685a2aa724955b13cc65
Joe McKie <joe@joemck.ie>
2015-08-09 23:17:19 +0100

    Added Travis build script to generate documentation

---
+++ package/scripts/deploy-ghpages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+( cd ../;
+git checkout gh-pages;
+cd package;
+grunt esdoc;
+git config user.name "Travis-CI";
+git config user.email "travis@joemck.ie";
+git add -A .;
+git commit -m "Documentation generated";
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
