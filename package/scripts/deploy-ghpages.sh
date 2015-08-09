b2cd0d2cf3c3cbfd5f849f0aa1a9a0fa4106e0fc
Joe McKie <joe@joemck.ie>
2015-08-10 00:28:00 +0100

    Fixed git checkout bug

--- package/scripts/deploy-ghpages.sh
+++ package/scripts/deploy-ghpages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-( cd ../;
-	git fetch && git checkout gh-pages;
-	cd package;
-	npm install;
-	grunt esdoc;
-	git config user.name "Travis-CI";
-	git config user.email "travis@joemck.ie";
-	git add -A .;
-	git commit -m "Documentation generated";
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+cd ../ &&
+git fetch origin gh-pages &&
+git checkout FETCH_HEAD &&
+cd package &&
+npm install &&
+grunt esdoc &&
+git config user.name "Travis-CI" &&
+git config user.email "travis@joemck.ie" &&
+git add -A . &&
+git commit -m "Documentation generated" &&
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
