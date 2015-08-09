1c008521c8e17afc9f4bcc889aa16f18f839c329
Joe McKie <joe@joemck.ie>
2015-08-09 23:36:11 +0100

    Added `git fetch` step, as the gh-pages branch wasn't accessible from travis

--- package/scripts/deploy-ghpages.sh
+++ package/scripts/deploy-ghpages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ( cd ../;
-git checkout gh-pages;
-cd package;
-npm install;
-grunt esdoc;
-git config user.name "Travis-CI";
-git config user.email "travis@joemck.ie";
-git add -A .;
-git commit -m "Documentation generated";
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+	git fetch && git checkout gh-pages;
+	cd package;
+	npm install;
+	grunt esdoc;
+	git config user.name "Travis-CI";
+	git config user.email "travis@joemck.ie";
+	git add -A .;
+	git commit -m "Documentation generated";
+	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
