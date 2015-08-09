051079cb50712b7a5dd703b89dee8a71ce761b17
Joe McKie <joe@joemck.ie>
2015-08-10 00:59:29 +0100

    Fixed documentation installation directory and silenced git

--- package/scripts/deploy-ghpages.sh
+++ package/scripts/deploy-ghpages.sh
@@ -8,4 +8,4 @@ git config user.name "Travis-CI" &&
 git config user.email "travis@joemck.ie" &&
 git add -A . &&
 git commit -m "Documentation generated" &&
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
